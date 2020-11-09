97f97ab59c34835261d82454c43770af2569b8f5
bduaart <contato.bduaart@gmail.com>
2020-11-09 08:03:07 -0300

    Updated

---
+++ Classes/Imposto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tingle.Classes
+{
+    class Imposto
+    {
+        public List<Prod> produto { get; set; }
+        public List<ICMS> ICMS { get; set; }
+        public List<PIS> PIS { get; set; }
+        public List<COFINS> COFINS { get; set; }
+    }
+}
---
+++ Classes/Nfe.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace Tingle
+{
+    class Nfe
+    {
+        //Atributos para abrir o Esquema XML
+        public static string Path { get; set; }
+        public static DataSet XMLSchema = new DataSet();
+
+        //Abrir o arquivo XML
+        public static void Open()
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog
+            {
+                RestoreDirectory = true,
+                ReadOnlyChecked = true,
+                CheckPathExists = true,
+                CheckFileExists = true,
+                Title = "Insira seu documento",
+                Filter = "Arquivo XML|*.xml"
+
+            };
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Path = fileDialog.FileName.ToString();
+            }
+        }
+
+        //Ler o Esquema XML com a função do DataSet
+        public static void Read()
+        {
+            XMLSchema.ReadXml(Path);
+        }
+    }
+}
